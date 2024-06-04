25b1249c01f22678e5e34d3c2194884182650060
anargya-anubhawa <anargyaprima2@gmail.com>
2024-06-04 12:49:44 +0700

    update

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -3,9 +3,9 @@ import createDebug from 'debug';
 
 import { author, name, version } from '../../bot.json';
 
-const debug = createDebug('bot:about_command');
+const debug = createDebug('bot:help_command');
 
-const about = () => async (ctx: Context) => {
+const help = () => async (ctx: Context) => {
   const message = `This is help message. `;
   debug(`Triggered "about" command with message \n${message}`);
   await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
--- src/text/start.ts
+++ src/text/start.ts
@@ -1,18 +1,18 @@
 import { Context } from 'telegraf';
 import createDebug from 'debug';
 
-const debug = createDebug('bot:greeting_text');
+const debug = createDebug('bot:start_text');
 
 const replyToMessage = (ctx: Context, messageId: number, string: string) =>
   ctx.reply(string, {
     reply_parameters: { message_id: messageId },
   });
 
-const greeting = () => async (ctx: Context) => {
+const start = () => async (ctx: Context) => {
   debug('Triggered "start" text command');
 
   const messageId = ctx.message?.message_id;
-  const userName = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name}`;
+  const userName = `${ctx.message?.from.first_name}`;
 
   if (messageId) {
     await replyToMessage(ctx, messageId, `Hello, ${userName}!`);
